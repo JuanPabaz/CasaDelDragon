e86600dede2773dc0798303f2d1cf84a06da233c
JuanPabaz <jpgthf@gmail.com>
2024-07-10 14:41:29 -0500

    Add findAllAliados

--- casaDragon/src/main/java/com/got/casaDragon/service/AliadoService.java
+++ casaDragon/src/main/java/com/got/casaDragon/service/AliadoService.java
@@ -1,9 +1,12 @@
 package com.got.casaDragon.service;
 
 import com.got.casaDragon.models.Aliado;
+import com.got.casaDragon.models.Dragon;
 import com.got.casaDragon.repositories.AliadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AliadoService {
@@ -14,6 +17,14 @@ public class AliadoService {
     public Aliado guardarAliado(Aliado aliado) throws Exception{
         try {
             return aliadoRepository.save(aliado);
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    public List<Aliado> buscarTodosAliados() throws Exception{
+        try {
+            return aliadoRepository.findAll();
         }catch (Exception error){
             throw new Exception(error.getMessage());
         }
