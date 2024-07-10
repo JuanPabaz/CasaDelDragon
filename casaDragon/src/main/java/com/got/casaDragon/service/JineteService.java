98dcb50c0346dc858de5bd0d8d3b1b21ff0be5b9
JuanPabaz <jpgthf@gmail.com>
2024-07-10 14:41:51 -0500

    Add findAllJinetes

--- casaDragon/src/main/java/com/got/casaDragon/service/JineteService.java
+++ casaDragon/src/main/java/com/got/casaDragon/service/JineteService.java
@@ -1,9 +1,12 @@
 package com.got.casaDragon.service;
 
+import com.got.casaDragon.models.Dragon;
 import com.got.casaDragon.models.Jinete;
 import com.got.casaDragon.repositories.JineteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JineteService {
@@ -14,6 +17,14 @@ public class JineteService {
     public Jinete guardarJinete(Jinete jinete) throws Exception{
         try {
             return jineteRepository.save(jinete);
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    public List<Jinete> buscarTodosJinetes() throws Exception{
+        try {
+            return jineteRepository.findAll();
         }catch (Exception error){
             throw new Exception(error.getMessage());
         }
