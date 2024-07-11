5de2fd7ff619562f36c32e9777eafef6887478ec
JuanPabaz <jpgthf@gmail.com>
2024-07-11 13:53:41 -0500

    Implement MensajeServicios message in aliado service

--- casaDragon/src/main/java/com/got/casaDragon/service/AliadoService.java
+++ casaDragon/src/main/java/com/got/casaDragon/service/AliadoService.java
@@ -1,12 +1,15 @@
 package com.got.casaDragon.service;
 
+import com.got.casaDragon.helpers.MensajeServicios;
 import com.got.casaDragon.models.Aliado;
 import com.got.casaDragon.models.Dragon;
+import com.got.casaDragon.models.Jinete;
 import com.got.casaDragon.repositories.AliadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AliadoService {
@@ -27,6 +30,19 @@ public class AliadoService {
             return aliadoRepository.findAll();
         }catch (Exception error){
             throw new Exception(error.getMessage());
+        }
+    }
+
+    public Aliado buscarAliadoPorId(Integer id) throws Exception{
+        try {
+            Optional<Aliado> aliadoOptional = aliadoRepository.findById(id);
+            if (aliadoOptional.isPresent()){
+                return aliadoOptional.get();
+            }else {
+                throw new Exception(MensajeServicios.ALIADO_NO_ENCONTRADO.getMensaje());
+            }
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
         }
     }
 
