368c9ae6d6aff703c7217448a360ccccde1c14b4
JuanPabaz <jpgthf@gmail.com>
2024-07-11 13:53:50 -0500

    Implement MensajeServicios message in jinete service

--- casaDragon/src/main/java/com/got/casaDragon/service/JineteService.java
+++ casaDragon/src/main/java/com/got/casaDragon/service/JineteService.java
@@ -1,5 +1,6 @@
 package com.got.casaDragon.service;
 
+import com.got.casaDragon.helpers.MensajeServicios;
 import com.got.casaDragon.models.Dragon;
 import com.got.casaDragon.models.Jinete;
 import com.got.casaDragon.repositories.JineteRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JineteService {
@@ -27,6 +29,19 @@ public class JineteService {
             return jineteRepository.findAll();
         }catch (Exception error){
             throw new Exception(error.getMessage());
+        }
+    }
+
+    public Jinete buscarJinetePorId(Integer id) throws Exception{
+        try {
+            Optional<Jinete> jineteOptional = jineteRepository.findById(id);
+            if (jineteOptional.isPresent()){
+                return jineteOptional.get();
+            }else {
+                throw new Exception(MensajeServicios.JINETE_NO_ENCONTRADO.getMensaje());
+            }
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
         }
     }
 
