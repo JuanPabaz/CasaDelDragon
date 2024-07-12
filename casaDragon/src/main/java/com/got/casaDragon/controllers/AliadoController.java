1a930542dec550116150777a6263a9f4217c1684
JuanPabaz <jpgthf@gmail.com>
2024-07-12 10:54:49 -0500

    Implement HttpStatus in all the AliadoController methods

--- casaDragon/src/main/java/com/got/casaDragon/controllers/AliadoController.java
+++ casaDragon/src/main/java/com/got/casaDragon/controllers/AliadoController.java
@@ -3,6 +3,7 @@ package com.got.casaDragon.controllers;
 import com.got.casaDragon.models.Aliado;
 import com.got.casaDragon.service.AliadoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,27 +16,47 @@ public class AliadoController {
 
     @GetMapping()
     public ResponseEntity<?> encontrarTodosAliado() throws Exception{
-        return ResponseEntity.ok(aliadoService.buscarTodosAliados());
+        try{
+            return ResponseEntity.status(HttpStatus.FOUND).body(aliadoService.buscarTodosAliados());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @GetMapping("/{idAliado}")
     public ResponseEntity<?> buscarAliado(@PathVariable(name = "idAliado") Integer id) throws Exception{
-        return ResponseEntity.ok(aliadoService.buscarAliadoPorId(id));
+        try{
+            return ResponseEntity.status(HttpStatus.FOUND).body(aliadoService.buscarAliadoPorId(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @PostMapping()
     public ResponseEntity<?> guardarAliado(@RequestBody Aliado aliado) throws Exception{
-        return ResponseEntity.ok(aliadoService.guardarAliado(aliado));
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(aliadoService.guardarAliado(aliado));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @PutMapping("/{idAliado}")
     public ResponseEntity<?> editarAliado(@PathVariable(name = "idAliado") Integer id,@RequestBody Aliado nuevoAliado) throws Exception{
-        return ResponseEntity.ok(aliadoService.editarAliado(id,nuevoAliado));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(aliadoService.editarAliado(id,nuevoAliado));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{idAliado}")
     public ResponseEntity<?> eliminarAliado(@PathVariable(name = "idAliado") Integer id) throws Exception{
-        return ResponseEntity.ok(aliadoService.eliminarAliado(id));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(aliadoService.eliminarAliado(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
 
