b4d5c9b91a124d0cb09331e7203b09135d3823a2
JuanPabaz <jpgthf@gmail.com>
2024-07-12 10:48:24 -0500

    Implement HttpStatus in all the JineteController methods

--- casaDragon/src/main/java/com/got/casaDragon/controllers/JineteController.java
+++ casaDragon/src/main/java/com/got/casaDragon/controllers/JineteController.java
@@ -3,6 +3,7 @@ package com.got.casaDragon.controllers;
 import com.got.casaDragon.models.Jinete;
 import com.got.casaDragon.service.JineteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,27 +15,47 @@ public class JineteController {
     private JineteService jineteService;
 
     @GetMapping()
-    public ResponseEntity<?> encontrarTodosJinete() throws Exception{
-        return ResponseEntity.ok(jineteService.buscarTodosJinetes());
+    public ResponseEntity<?> encontrarTodosJinete(){
+        try {
+            return ResponseEntity.status(HttpStatus.FOUND).body(jineteService.buscarTodosJinetes());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @GetMapping("/{idJinete}")
-    public ResponseEntity<?> buscarJinete(@PathVariable(name = "idJinete") Integer id) throws Exception{
-        return ResponseEntity.ok(jineteService.buscarJinetePorId(id));
+    public ResponseEntity<?> buscarJinete(@PathVariable(name = "idJinete") Integer id){
+        try {
+            return ResponseEntity.status(HttpStatus.FOUND).body(jineteService.buscarJinetePorId(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @PostMapping()
-    public ResponseEntity<?> guardarJinete(@RequestBody Jinete jinete) throws Exception{
-        return ResponseEntity.ok(jineteService.guardarJinete(jinete));
+    public ResponseEntity<?> guardarJinete(@RequestBody Jinete jinete){
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(jineteService.guardarJinete(jinete));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @PutMapping("/{idJinete}")
-    public ResponseEntity<?> editarJinete(@PathVariable(name = "idJinete") Integer id,@RequestBody Jinete nuevoJinete) throws Exception{
-        return ResponseEntity.ok(jineteService.editarJinete(id,nuevoJinete));
+    public ResponseEntity<?> editarJinete(@PathVariable(name = "idJinete") Integer id,@RequestBody Jinete nuevoJinete){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(jineteService.editarJinete(id,nuevoJinete));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{idJinete}")
-    public ResponseEntity<?> eliminarJinete(@PathVariable(name = "idJinete") Integer id) throws Exception{
-        return ResponseEntity.ok(jineteService.eliminarJinete(id));
+    public ResponseEntity<?> eliminarJinete(@PathVariable(name = "idJinete") Integer id){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(jineteService.eliminarJinete(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
