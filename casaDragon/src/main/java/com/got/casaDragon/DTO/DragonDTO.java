24274e307d7c9189a3e0c06dcd6ab6317e2af1bc
JuanPabaz <jpgthf@gmail.com>
2024-07-15 11:05:52 -0500

    Add DragonDTO

---
+++ casaDragon/src/main/java/com/got/casaDragon/DTO/DragonDTO.java
@@ -0,0 +1,15 @@
+package com.got.casaDragon.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class DragonDTO {
+
+    private String nombreDragon;
+    private Integer edad;
+
+}
