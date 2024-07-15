221efbdc089d53f19eb1b55e28cda3dd7ac373a8
JuanPabaz <jpgthf@gmail.com>
2024-07-15 11:05:43 -0500

    Add JineteDTO

---
+++ casaDragon/src/main/java/com/got/casaDragon/DTO/JineteDTO.java
@@ -0,0 +1,15 @@
+package com.got.casaDragon.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JineteDTO {
+
+    private String nombreJinete;
+    private Integer edadJinete;
+
+}
