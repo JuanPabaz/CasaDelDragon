ff8540ef4c78fb0d9a15f608bf7baf774710cfa2
JuanPabaz <jpgthf@gmail.com>
2024-07-19 11:32:23 -0500

    feat: Implement guardarDragonDevuelveDragonDto test

--- casaDragon/src/test/java/com/got/casaDragon/controllers/DragonControllerTest.java
+++ casaDragon/src/test/java/com/got/casaDragon/controllers/DragonControllerTest.java
@@ -4,10 +4,13 @@ import com.got.casaDragon.DTO.DragonDTO;
 import com.got.casaDragon.models.Dragon;
 import com.got.casaDragon.service.DragonService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -26,9 +29,23 @@ class DragonControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    @Test
     public void guardarDragonDevuelveDragonDto() throws Exception{
-        //arrange
+        //Arrange
         DragonDTO dragonDTO = new DragonDTO();
-        when(dragonService.agregarDragon(any(Dragon.class)));
+        //Cuando algo se ejecuta que retorno
+        when(dragonService.agregarDragon(any(Dragon.class))).thenReturn(dragonDTO);
+
+        //Act (Donde llamo al metodo que quiero probar)
+        Dragon dragon = new Dragon();
+        ResponseEntity<?> response = dragonController.guardarDragon(dragon);
+
+        //Assert
+        assertEquals(HttpStatus.CREATED,response.getStatusCode());
+        assertEquals(dragonDTO,response.getBody());
+    }
+
+    public void guardarDragonOcurreUnErrorDevuelveUnMensaje() throws Exception{
+
     }
 }
